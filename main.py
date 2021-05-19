3aa6be527bf36f330bbcb6823f519bf4411c2f53
romanpovzyk <roman.povzyk@gmail.com>
2021-05-19 17:45:37 +0300

    second commit

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ a_numb = int("".join(a_string))
 while str(b) in str(a_numb):
     b = str(b) + '1'
 
-print(len(str(b))-1)
+print(len(str(b)) - 1)
 
 print(f'Одиниць — {a.count(1)}')
 print(f'Нулів — {a.count(0)}')
